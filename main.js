3f31bb5f1f629fb3c332676a20bc63efd3e70d69
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-07-12 09:59:42 -0500

    Lógica terminada

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-const ciudad = 'Chilpancingo'
-const URL = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=616629f9acdc3b22b8b09553e632e5da`;
+let ciudad = '';
+
+const getInput = () =>{
+    const input = document.getElementById("input").value;
+    ciudad = input;
+    getData();
+}
 
 const getData = async () => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=616629f9acdc3b22b8b09553e632e5da`;
     const peticion = await fetch(URL);
     const data = await peticion.json();
     console.log(data)
 }
-
-getData();
