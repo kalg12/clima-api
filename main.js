04e1833347176e18b27900927bb776bbc493195f
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-07-12 10:25:51 -0500

    Reto finalizado

--- main.js
+++ main.js
@@ -11,4 +11,23 @@ const getData = async () => {
     const peticion = await fetch(URL);
     const data = await peticion.json();
     console.log(data)
+    const table = document.getElementById("table").innerHTML=`
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Ciudad</th>
+      <th scope="col">Clima</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>${data.name}</td>
+      <td>${data.main.temp/10}</td>
+    </tr>
+  </tbody>
+</table>
+    `
 }
