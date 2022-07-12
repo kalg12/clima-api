5ff585523fa6cac778b742082869822477538764
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-07-12 09:27:36 -0500

    Actualizaciones

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-import { getData } from "./src/api/clima.js";
+const ciudad = 'Chilpancingo'
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=616629f9acdc3b22b8b09553e632e5da`;
 
+const getData = async () => {
+    const peticion = await fetch(URL);
+    const data = await peticion.json();
+    console.log(data)
+}
 
 getData();
--- src/api/clima.js
+++
@@ -1,8 +0,0 @@
-const ciudad = 'Acapulco'
-const URL = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=616629f9acdc3b22b8b09553e632e5da`;
-
-export const getData = async () => {
-    const peticion = await fetch(URL);
-    const data = await peticion.json();
-    console.log(data)
-}
